22a24e04b0264d630568148fc878603366fb7e78
Park,JongBum <jbpark03@gmail.com>
2021-09-20 10:18:14 +0900

    set static resource configutaion
    visit http://localhost:8080/store/img/home32.png

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # bumsoap - handmade soap online store
 
-- Latest modification date: 2021-09-19
+- Latest modification date: 2021-09-20
 - Things to do next: create a welcome page
+- [Development history note](https://docs.google.com/spreadsheets/d/1MuOT42wZYtMyvM6YXbqN6iuAugS3YmG95FgcCvE1KLc/edit?usp=sharing)
 - Launching Date: 2021-09-19
--- src/main/java/com/bumsoap/store/config/WebApplicationConfig.java
+++ src/main/java/com/bumsoap/store/config/WebApplicationConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -13,6 +14,13 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan("com.bumsoap.store")
 public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/img/**")
+			.addResourceLocations("/resources/images/");
+	}
+
     @Override
     public void configureDefaultServletHandling(
         DefaultServletHandlerConfigurer configurer) {
